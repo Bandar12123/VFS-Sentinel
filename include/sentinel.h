4665fe19dedb5ed955e2ef5b9134c2cd7ca0f0a6
Bandar12123 <bandarhatim11@gmail.com>
2026-09-23 01:06:52 +0300

    Add _GNU_SOURCE, system headers, and canary macros to sentinel.h

--- include/sentinel.h
+++ include/sentinel.h
@@ -1,9 +1,15 @@
 #ifndef SENTINEL_H
 #define SENTINEL_H
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+
+
+#define CANARY_PREFIX "000_canary_"
+#define CANARY_SUFFIX ".docx"
+#define BUFFER_SIZE 4096
 
 #endif
