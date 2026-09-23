3f9b4081f02629d00cefff58b09ed5ad71a54253
Bandar12123 <bandarhatim11@gmail.com>
2026-09-23 06:31:34 +0300

    Implement creat_canary_file function

---
+++ src/canary.c
@@ -0,0 +1,26 @@
+#include "canary.h"
+
+int create_canary_file(const char *target_dir, char *out_canary_path){
+    if (!target_dir || !out_canary_path){
+        return -1;
+    }
+
+    snprintf(out_canary_path, BUFFER_SIZE, "%s/%s%s", target_dir, CANARY_PREFIX, CANARY_SUFFIX);
+
+    int fd = open(out_canary_path, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+    if (fd < 0){
+        perror("Failed to create canary file");
+        return -1;
+    }
+
+    const char dummy_docx_header[] = "PK\x03\x04_VFS_SENTINEL_CANARY_PROTECTED_DATA";
+    if(write(fd, dummy_docx_header, sizeof(dummy_docx_header) -1) < 0){
+        perror("Failed to write to canary file");
+        close(fd);
+        return -1;
+    }
+    close(fd);
+    printf("Canary file created at: %s\n", out_canary_path);
+    return 0;
+
+}
