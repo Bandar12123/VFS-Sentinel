75c787d6b3333085c4a23c8e74f8cc5f536e189c
Bandar12123 <bandarhatim11@gmail.com>
2026-09-25 01:46:39 +0300

    Continue backup_file function

--- src/backup.c
+++ src/backup.c
@@ -17,4 +17,14 @@ int backup_file(int src_fd) {
     char backup_path[256];
     snprintf(backup_path, sizeof(backup_path), "%s/%lu.bak",
             BACKUP_DIR, (unsigned long)st.st_ino);
+
+    int dest_fd = open(backup_path, O_CREAT | O_EXCL | O_WRONLY | O_NOFOLLOW, 0600);
+    if (dest_fd < 0) {
+        return (errno == EEXIST) ? 0 : -1;
+    }
+
+    off_t offset = 0;
+    ssize_t copied = copy_file_range(src_fd, &offset, dest_fd, NULL, st.st_size, 0);
+    close(dest_fd);
+    return (copied >= 0) ? 0 : -1;
 }
