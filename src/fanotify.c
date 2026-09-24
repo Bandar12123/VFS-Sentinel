50588a7fa3217bd731eaac034310f35540b4c5a5
Bandar12123 <bandarhatim11@gmail.com>
2026-09-24 05:49:46 +0300

    Implement get_filepath_from_fd function

--- src/fanotify.c
+++ src/fanotify.c
@@ -15,10 +15,22 @@ int mark_directory(int fan_fd, const char *dir_path){
                             FAN_OPEN_PERM | FAN_EVENT_ON_CHILD,
                             AT_FDCWD,
                             dir_path);
-    if (ret == 1){
+    if (ret == -1){
         perror("[ERROR] fanotify_mark failed");
         return -1;
     }
     printf("[VFS-Sentinel] Monitoring active on directory: %s\n", dir_path);
     return 0;
+}
+
+static void get_filepath_from_fd(int fd, char *path_buffer, size_t size){
+    char proc_path[64];
+    snprintf(proc_path, sizeof(proc_path), "/proc/self/fd/%d", fd);
+    ssize_t len = readlink(proc_path, path_buffer, size - 1);
+    if (len != -1){
+        path_buffer[len] = '\0';
+    } else {
+        snprintf(path_buffer, size, "UNKNOWN");
+    }
+
 }
